d6c73e42387182200376677cdd8ab7c91c26a52a
jxsoren <jxsoren@gmail.com>
2024-02-01 14:18:29 -0700

    added comments

--- parser.rb
+++ parser.rb
@@ -2,10 +2,16 @@ require 'json'
 
 puts "Enter JSON-like hash"
 
+# Store standard input
 response = gets.chomp
+
+# Create new string with rockets replaced with semicolons
 semi_to_rocket = response.gsub('=>', ':', )
+
+# Create new string with nil converted to null, only if it's a standalone
 json_compatible_string = semi_to_rocket.gsub(/\bnil\b/, 'null')
 
+# Try to parse JSON w/ the above string; else, re-route err to standard output
 begin
   parsed_json = JSON.parse(json_compatible_string)
 rescue JSON::ParseError => e
@@ -13,5 +19,7 @@ rescue JSON::ParseError => e
   exit
 end
 
+# Clear console and pretty print formatted JSOn
+puts `clear`
 json_output = JSON.pretty_generate(parsed_json)
 puts json_output
